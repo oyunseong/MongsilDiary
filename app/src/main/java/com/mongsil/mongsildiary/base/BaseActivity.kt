911e44283c032f93fd64919ae9e7f492207619ad
Administrator <gkrlsanjf@naver.com>
2022-03-12 17:22:23 +0900

    status bar background 투명색으로 변경

--- app/src/main/java/com/mongsil/mongsildiary/base/BaseActivity.kt
+++ app/src/main/java/com/mongsil/mongsildiary/base/BaseActivity.kt
@@ -1,8 +1,12 @@
 package com.mongsil.mongsildiary.base
 
+import android.app.Activity
+import android.graphics.Color
+import android.os.Build
 import android.os.Bundle
 import android.util.Log
 import android.view.LayoutInflater
+import android.view.View
 import android.view.Window
 import android.view.WindowManager
 import android.widget.Toast
@@ -22,13 +26,23 @@ abstract class BaseActivity<B: ViewBinding>(val bindingFactory: (LayoutInflater)
         Log.d(tag, "++onCreate")
         _binding = bindingFactory(layoutInflater)
         setContentView(binding.root)
-
-        // 상태바 배경 색상 변경
-        val window : Window = window
-        window.clearFlags(WindowManager.LayoutParams.FLAG_TRANSLUCENT_STATUS)
-        window.addFlags(WindowManager.LayoutParams.FLAG_DRAWS_SYSTEM_BAR_BACKGROUNDS)
-        window.statusBarColor = ContextCompat.getColor(this, R.color.main_background)
+        makeStatusBarTransparent()
     }
+
+    fun Activity.makeStatusBarTransparent() {
+        window.apply {
+            clearFlags(WindowManager.LayoutParams.FLAG_TRANSLUCENT_STATUS)
+            addFlags(WindowManager.LayoutParams.FLAG_DRAWS_SYSTEM_BAR_BACKGROUNDS)
+            if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.M) {
+                decorView.systemUiVisibility =
+                    View.SYSTEM_UI_FLAG_LAYOUT_FULLSCREEN or View.SYSTEM_UI_FLAG_LIGHT_STATUS_BAR
+            } else {
+                decorView.systemUiVisibility = View.SYSTEM_UI_FLAG_LAYOUT_FULLSCREEN
+            }
+            statusBarColor = Color.TRANSPARENT
+        }
+    }
+
     override fun onDestroy() {
         super.onDestroy()
         Log.d(tag, "++onDestroy")
