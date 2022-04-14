0600f168b49730c4410d49bbb47db942b7891032
haeseong5 <dhgotjd1234@naver.com>
2022-04-15 02:40:55 +0900

    commit

--- app/src/main/java/com/mongsil/mongsildiary/ui/calendar/BottomSheetFragment.kt
+++ app/src/main/java/com/mongsil/mongsildiary/ui/calendar/BottomSheetFragment.kt
@@ -11,25 +11,30 @@ import android.widget.NumberPicker
 import android.widget.TextView
 import android.widget.Toast
 import androidx.databinding.DataBindingUtil
+import com.google.android.material.bottomsheet.BottomSheetDialog
 import com.google.android.material.bottomsheet.BottomSheetDialogFragment
 import com.mongsil.mongsildiary.R
 import com.mongsil.mongsildiary.databinding.DialogDatepickerBinding
 
 class BottomSheetFragment : BottomSheetDialogFragment() {
 
-    override fun onCreate(savedInstanceState: Bundle?) {
-        super.onCreate(savedInstanceState)
-
+    override fun onCreateView(
+        inflater: LayoutInflater,
+        container: ViewGroup?,
+        savedInstanceState: Bundle?
+    ): View? {
+        return inflater.inflate(R.layout.dialog_datepicker,container,false)
     }
-
 
     override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
         super.onViewCreated(view, savedInstanceState)
 
         val year: NumberPicker = view.findViewById(R.id.yearpicker_datepicker)
         val month: NumberPicker = view.findViewById(R.id.monthpicker_datepicker)
-        val cancelBtn: TextView = view.findViewById(R.id.cancel_btn)
-        val confirmBtn: TextView = view.findViewById(R.id.confirm_btb)
+        val cancel: TextView = view.findViewById(R.id.cancel_btn)
+        val save: TextView = view.findViewById(R.id.confirm_btb)
+        val year_tv: TextView = view.findViewById(R.id.year_tv)
+        val month_tv: TextView = view.findViewById(R.id.month_tv)
 
         //  순환 안되게 막기
         year.wrapSelectorWheel = false
@@ -39,36 +44,28 @@ class BottomSheetFragment : BottomSheetDialogFragment() {
         year.descendantFocusability = NumberPicker.FOCUS_BLOCK_DESCENDANTS
         month.descendantFocusability = NumberPicker.FOCUS_BLOCK_DESCENDANTS
 
-
         //  최소값 설정
         year.minValue = 2019
         month.minValue = 1
 
         //  최대값 설정
-        year.maxValue = 2020
+        year.maxValue = 2030
         month.maxValue = 12
 
-        year.setOnValueChangedListener { _, _, newVal ->
-
-
-        }
-
-
-
         //  취소 버튼 클릭 시
-        confirmBtn.setOnClickListener {
-            Toast.makeText(context, "취소 클릭", Toast.LENGTH_SHORT).show()
+        cancel.setOnClickListener {
             dismiss()
+            Toast.makeText(requireContext(),"${year.value}",Toast.LENGTH_SHORT).show()
         }
 
         //  완료 버튼 클릭 시
-//        save.setOnClickListener {
-//            year_textview_statsfrag.text = (year.value).toString() + "년"
-//            month_textview_statsfrag.text = (month.value).toString() + "월"
-//
-//            dialog.dismiss()
-//            dialog.cancel()
-//        }
+        save.setOnClickListener {
+            year_tv.text = (year.value).toString() + "년"
+            month_tv.text = (month.value).toString() + "월"
+
+            dismiss()
+
+        }
 
     }
 }
--- app/src/main/java/com/mongsil/mongsildiary/ui/calendar/CalendarFragment.kt
+++ app/src/main/java/com/mongsil/mongsildiary/ui/calendar/CalendarFragment.kt
@@ -1,8 +1,6 @@
 package com.mongsil.mongsildiary.ui.calendar
 
-import android.app.AlertDialog
 import android.graphics.Color
-import android.os.Build
 import android.os.Bundle
 import android.view.LayoutInflater
 import android.view.View
@@ -17,7 +15,6 @@ import com.mongsil.mongsildiary.databinding.FragmentCalendarBinding
 
 class CalendarFragment : BaseFragment<FragmentCalendarBinding>() {
 
-
     override fun getFragmentBinding(
         inflater: LayoutInflater,
         container: ViewGroup?
@@ -27,68 +24,16 @@ class CalendarFragment : BaseFragment<FragmentCalendarBinding>() {
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-
-
-
     }
+
     override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
         super.onViewCreated(view, savedInstanceState)
-        setBottomSheetDialog()
-
-    }
-
-    fun setBottomSheetDialog(){
-        val bottomSheetView = layoutInflater.inflate(R.layout.dialog_datepicker,null)
-        val bottomSheetDialog = BottomSheetDialog(requireContext())
-        bottomSheetDialog.setContentView(bottomSheetView)
+        val bottomSheetFragment = BottomSheetFragment()
 
         binding.title.setOnClickListener {
-
-            val year: NumberPicker = bottomSheetView.findViewById(R.id.yearpicker_datepicker)
-            val month: NumberPicker = bottomSheetView.findViewById(R.id.monthpicker_datepicker)
-            val cancel: TextView = bottomSheetView.findViewById(R.id.cancel_btn)
-            val save: TextView = bottomSheetView.findViewById(R.id.confirm_btb)
-            val year_tv: TextView = bottomSheetView.findViewById(R.id.year_tv)
-            val month_tv: TextView = bottomSheetView.findViewById(R.id.month_tv)
-
-
-            //  순환 안되게 막기
-            year.wrapSelectorWheel = false
-            month.wrapSelectorWheel = false
-
-            //  editText 설정 해제
-            year.descendantFocusability = NumberPicker.FOCUS_BLOCK_DESCENDANTS
-            month.descendantFocusability = NumberPicker.FOCUS_BLOCK_DESCENDANTS
-
-            //  최소값 설정
-            year.minValue = 2019
-            month.minValue = 1
-
-            //  최대값 설정
-            year.maxValue = 2030
-            month.maxValue = 12
-
-//            year.displayedValues= arrayOf("2021","2022","2023","2024","2025")
-
-            //  취소 버튼 클릭 시
-            cancel.setOnClickListener {
-                bottomSheetDialog.dismiss()
-                bottomSheetDialog.cancel()
-                Toast.makeText(requireContext(),"${year.value}",Toast.LENGTH_SHORT).show()
-            }
-
-            //  완료 버튼 클릭 시
-            save.setOnClickListener {
-                year_tv.text = (year.value).toString() + "년"
-                month_tv.text = (month.value).toString() + "월"
-
-                bottomSheetDialog.dismiss()
-                bottomSheetDialog.cancel()
-            }
-
-//
-            bottomSheetDialog.show()
+            bottomSheetFragment.show(parentFragmentManager,bottomSheetFragment.tag)
         }
+
     }
 
 }
