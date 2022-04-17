d0558148c721611c0e5fc98408bc9e455cf16b9c
gkrlsanjf <gkrlsanjf@naver.com>
2022-04-18 04:31:05 +0900

    commit

--- app/src/main/java/com/mongsil/mongsildiary/ui/home/TimeSlotAdapter.kt
+++ app/src/main/java/com/mongsil/mongsildiary/ui/home/TimeSlotAdapter.kt
@@ -1,5 +1,6 @@
 package com.mongsil.mongsildiary.ui.home
 
+import android.content.Context
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
@@ -13,10 +14,18 @@ import com.mongsil.mongsildiary.databinding.ItemTimeslotListBinding
 import com.mongsil.mongsildiary.model.Emotion
 
 class TimeSlotAdapter(
+    val context: Context,
     private val dataSet: ArrayList<List<Emotion>>,
-    private val onItemClickListener: OnItemClickListener
+    private val onItemClickListener: ViewHolder.OnItemClickListener
 ) :
     RecyclerView.Adapter<TimeSlotAdapter.ViewHolder>() {
+    val indexList = Array(dataSet.size) { false }   // 리스트를 전체 false로 초기화
+    var index = 0 // 선택된 item's index
+
+    init {
+        if (dataSet.isNotEmpty())
+            indexList[0] = true
+    }
 
     override fun onCreateViewHolder(parent: ViewGroup, viewType: Int): ViewHolder {
         val binding =
@@ -27,25 +36,40 @@ class TimeSlotAdapter(
     override fun getItemCount(): Int = dataSet.size
 
     override fun onBindViewHolder(holder: ViewHolder, position: Int) {
-
-        val gridLayoutManager = GridLayoutManager(holder.itemView.context,3)
+        val asset = context.resources.getDrawable(
+            if (indexList[position]) R.drawable.fill_circle
+            else R.drawable.stoke_circle
+        )
 
         holder.itemView.setOnClickListener {
             onItemClickListener.onClick(it, position)
         }
 
         holder.bind(dataSet[position])
+//        holder.bind(asset)
     }
 
     class ViewHolder(private val binding: ItemTimeslotListBinding) :
         RecyclerView.ViewHolder(binding.root) {
+//            val dotsView : View = bindi
+
         fun bind(data: List<Emotion>) {
             binding.image.setImageResource(R.drawable.ic_emoticon_01)
             binding.text.text = "몽실이"
+//        }
         }
+
+        interface OnItemClickListener {
+            fun onClick(v: View, position: Int)
+        }
+
     }
 
-    interface OnItemClickListener {
-        fun onClick(v: View, position: Int)
+    fun setItemIndex(position: Int) {
+        indexList[index] = false
+        indexList[position] = true
+        index = position
+
+        notifyDataSetChanged()
     }
 }
--- app/src/main/java/com/mongsil/mongsildiary/ui/home/TimeSlotFragment.kt
+++ app/src/main/java/com/mongsil/mongsildiary/ui/home/TimeSlotFragment.kt
@@ -1,5 +1,7 @@
 package com.mongsil.mongsildiary.ui.home
 
+import HorizontalItemDecorator
+import VerticalItemDecorator
 import android.os.Bundle
 import android.text.Editable
 import android.text.TextWatcher
@@ -7,6 +9,10 @@ import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
 import androidx.navigation.findNavController
+import androidx.recyclerview.widget.GridLayoutManager
+import androidx.recyclerview.widget.LinearLayoutManager
+import androidx.recyclerview.widget.LinearSnapHelper
+import androidx.recyclerview.widget.RecyclerView
 import androidx.viewpager2.widget.ViewPager2
 import com.mongsil.mongsildiary.R
 import com.mongsil.mongsildiary.base.BaseFragment
@@ -18,11 +24,12 @@ import kotlin.collections.ArrayList
 
 class TimeSlotFragment : BaseFragment<FragmentTimeslotBinding>() {
     private val dataSet: ArrayList<List<Emotion>> = arrayListOf()
-    private val timeSlotAdapter = TimeSlotAdapter(dataSet, object : TimeSlotAdapter.OnItemClickListener{
-        override fun onClick(v: View, position: Int) {
-            showToast("${position}번째 item")
-        }
-    })
+    private val timeSlotAdapter =
+        TimeSlotAdapter(requireContext(),dataSet, object : TimeSlotAdapter.OnItemClickListener {
+            override fun onClick(v: View, position: Int) {
+                showToast("${position}번째 item")
+            }
+        })
 
     override fun getFragmentBinding(
         inflater: LayoutInflater,
@@ -43,21 +50,23 @@ class TimeSlotFragment : BaseFragment<FragmentTimeslotBinding>() {
     override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
         super.onViewCreated(view, savedInstanceState)
         fabViewModel.setFabState(false)
-        binding.viewpager2.orientation = ViewPager2.ORIENTATION_HORIZONTAL
-        binding.viewpager2.adapter = timeSlotAdapter
+        initRecycler()
+
+//        binding.viewpager2.orientation = ViewPager2.ORIENTATION_HORIZONTAL
+//        binding.viewpager2.adapter = timeSlotAdapter
 
         binding.confirmBtn.isEnabled = false    // 버튼 비활성화
         binding.confirmBtn.isClickable = false
 
-        binding.editText.addTextChangedListener(object : TextWatcher{
+        binding.editText.addTextChangedListener(object : TextWatcher {
             override fun beforeTextChanged(p0: CharSequence?, p1: Int, p2: Int, p3: Int) {
             }
 
             override fun onTextChanged(p0: CharSequence?, p1: Int, p2: Int, p3: Int) {
-                if(binding.editText.length() == 0){   // 2자리 미만이면
+                if (binding.editText.length() == 0) {   // 2자리 미만이면
                     binding.confirmBtn.isEnabled = false    // 버튼 비활성화
                     binding.confirmBtn.isClickable = false
-                }else{
+                } else {
                     binding.confirmBtn.isEnabled = true // 버튼 활성화
                     binding.confirmBtn.isClickable = true
                 }
@@ -68,19 +77,53 @@ class TimeSlotFragment : BaseFragment<FragmentTimeslotBinding>() {
 
         })
         binding.confirmBtn.setOnClickListener {
-            if(it.isEnabled) {
+            if (it.isEnabled) {
                 view?.findNavController().navigate(R.id.action_timeSlotFragment_to_homeFragment)
                 showToast("클릭 되었습니다.")
-            }else{
+            } else {
 
             }
         }
     }
+
     private fun addData() {
         for (i in 0..99) {
-            dataSet.add(listOf(Emotion(R.drawable.ic_emoticon_01,"몽실이")))
+            dataSet.add(listOf(Emotion(R.drawable.ic_emoticon_01, "몽실이")))
 //            dataSet.add(listOf("$i th main", "$i th sub"))
         }
+    }
+
+    private fun initRecycler() {
+        binding.recycler.layoutManager =
+            GridLayoutManager(context, 3, LinearLayoutManager.HORIZONTAL, false)
+//            LinearLayoutManager(requireContext(), GridLayoutManager.HORIZONTAL, false)
+        binding.recycler.adapter = timeSlotAdapter
+        binding.recycler.addItemDecoration(VerticalItemDecorator(10))
+        binding.recycler.addItemDecoration(HorizontalItemDecorator(10))
+
+        val snapHelper = LinearSnapHelper()
+        snapHelper.attachToRecyclerView(binding.recycler)
+        binding.recycler.onFlingListener = snapHelper
+
+        binding.recycler.addOnScrollListener(object :RecyclerView.OnScrollListener(){
+            override fun onScrollStateChanged(recyclerView: RecyclerView, newState: Int) {
+                super.onScrollStateChanged(recyclerView, newState)
+
+                timeSlotAdapter.setItemIndex(
+                    (recyclerView.layoutManager as LinearLayoutManager)
+                        .findFirstVisibleItemPosition()
+                )
+            }
+        })
+
+//        rv_main_index.addOnItemTouchListener(
+//            RecyclerItemClickListener(applicationContext, rv_main_index,
+//                object : RecyclerItemClickListener.OnItemClickListener {
+//                    override fun onItemClick(view: View, position: Int) {
+//                        rv_main_card.smoothScrollToPosition(position)
+//                    }
+//                })
+//        )
     }
 
 
