dd6f7b1ef7e6ce7b07a980a46266e8bfeb03dde6
gkrlsanjf <gkrlsanjf@naver.com>
2022-05-29 16:19:51 +0900

    SettingFragment 디자인 변경 및 버튼 반응 추가

--- app/src/main/java/com/mongsil/mongsildiary/ui/setting/SettingFragment.kt
+++ app/src/main/java/com/mongsil/mongsildiary/ui/setting/SettingFragment.kt
@@ -5,19 +5,30 @@ import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
 import androidx.fragment.app.Fragment
+import androidx.fragment.app.FragmentManager
+import androidx.navigation.findNavController
+import com.mongsil.mongsildiary.R
 import com.mongsil.mongsildiary.base.BaseFragment
+import com.mongsil.mongsildiary.base.ViewBindingFragment
 import com.mongsil.mongsildiary.databinding.FragmentSettingBinding
 
-class SettingFragment : Fragment() {
-    private var _binding: FragmentSettingBinding? = null
-    private val binding get() = _binding!!
+class SettingFragment : ViewBindingFragment<FragmentSettingBinding>() {
+    private val toolbarTitle: String = "Setting"
 
-    override fun onCreateView(
+    override fun getFragmentBinding(
         inflater: LayoutInflater,
-        container: ViewGroup?,
-        savedInstanceState: Bundle?
-    ): View {
-        _binding = FragmentSettingBinding.inflate(inflater, container, false)
-        return binding.root
+        container: ViewGroup?
+    ): FragmentSettingBinding {
+        return FragmentSettingBinding.inflate(inflater, container, false)
+    }
+
+    override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
+        super.onViewCreated(view, savedInstanceState)
+        binding.toolbar.title.text = toolbarTitle
+        binding.toolbar.uploadBtn.visibility = View.GONE
+
+        binding.pushBtn.setOnClickListener {
+            requireView().findNavController().navigate(R.id.action_settingFragment_to_pushAllowFragment)
+        }
     }
 }
