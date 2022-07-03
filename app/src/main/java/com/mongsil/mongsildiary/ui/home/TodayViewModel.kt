862f299b461bb36cf5578ca27896cffd591a7dff
Administrator <gkrlsanjf@naver.com>
2022-07-03 16:41:27 +0900

    TodayViewModel이 생성될 때 초기데이터 생성

--- app/src/main/java/com/mongsil/mongsildiary/ui/home/TodayViewModel.kt
+++ app/src/main/java/com/mongsil/mongsildiary/ui/home/TodayViewModel.kt
@@ -1,28 +1,31 @@
 package com.mongsil.mongsildiary.ui.home
 
-import android.app.Application
 import androidx.lifecycle.LiveData
 import androidx.lifecycle.MutableLiveData
 import androidx.lifecycle.ViewModel
 import androidx.lifecycle.viewModelScope
 import com.mongsil.mongsildiary.MyApplication
+import com.mongsil.mongsildiary.R
 import com.mongsil.mongsildiary.data.database.AppDatabase
+import com.mongsil.mongsildiary.data.database.entity.TimeSlot
 import com.mongsil.mongsildiary.domain.Slot
 import com.mongsil.mongsildiary.repository.DiaryRepository
+import com.mongsil.mongsildiary.ui.home.today.Emoticon
+import com.mongsil.mongsildiary.utils.Date
+import com.mongsil.mongsildiary.utils.printLog
 import kotlinx.coroutines.launch
-import java.util.*
 
 class TodayViewModel(
     private val repository: DiaryRepository = DiaryRepository(
         AppDatabase.getInstance(MyApplication.context).diaryDao()
     ),
 ) : ViewModel() {
-
     private val _slotData = MutableLiveData<List<Slot>>(listOf())
     val slotData: LiveData<List<Slot>>
         get() = _slotData
 
     init {
+        initData()
         getSlotData()
     }
 
@@ -35,8 +38,54 @@ class TodayViewModel(
 
     private fun getSlotData() {
         viewModelScope.launch {
-            val slots = repository.getSlotsByDate(Calendar.DAY_OF_MONTH.toLong())
+            val slots = repository.getSlotsByDate(Date().date)
+            val slotsTest = repository.getSlotsByDate2()
+            "slotsTest : $slotsTest".printLog("TodayViewModel")
             _slotData.value = slots
+            "slots : $slots".printLog("getSlotData")
         }
+    }
+
+    private fun initData() {
+        insert(
+            Slot(
+                Date().date, "아침",
+                TimeSlot.Morning, Emoticon(
+                    id = 2,
+                    image = R.drawable.ic_emoticon_03,
+                    name = "노랑"
+                )
+            )
+        )
+        insert(
+            Slot(
+                Date().date, "점심",
+                TimeSlot.Launch, Emoticon(
+                    id = 2,
+                    image = R.drawable.ic_emoticon_03,
+                    name = "노랑"
+                )
+            )
+        )
+        insert(
+            Slot(
+                Date().date, "저녁",
+                TimeSlot.Dinner, Emoticon(
+                    id = 2,
+                    image = R.drawable.ic_emoticon_03,
+                    name = "노랑"
+                )
+            )
+        )
+        insert(
+            Slot(
+                Date().date, "하루끝",
+                TimeSlot.EndOfTheDay, Emoticon(
+                    id = 2,
+                    image = R.drawable.ic_emoticon_03,
+                    name = "노랑"
+                )
+            )
+        )
     }
 }
