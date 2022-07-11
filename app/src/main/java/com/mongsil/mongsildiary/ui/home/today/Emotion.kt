c9ff2639a1486bc48ada16fb83f8f9872d513f02
gkrlsanjf <gkrlsanjf@naver.com>
2022-07-11 16:39:00 +0900

    디폴트 이모티콘 리스트 생성

--- app/src/main/java/com/mongsil/mongsildiary/ui/home/today/Emotion.kt
+++ app/src/main/java/com/mongsil/mongsildiary/ui/home/today/Emotion.kt
@@ -1,6 +1,7 @@
 package com.mongsil.mongsildiary.ui.home.today
 
 import android.os.Parcelable
+import com.mongsil.mongsildiary.R
 import kotlinx.android.parcel.Parcelize
 
 // TODO 아이템 클릭 효과
@@ -20,3 +21,20 @@ data class TodayEmoticon(
     val emoticon: Emoticon,
     val isSelected: Boolean = false
 )
+
+object DataProvider {
+
+    fun getEmoticonList(): List<Emoticon> {
+        return listOf<Emoticon>(
+            Emoticon(0, R.drawable.ic_emoticon_01, "몽실이", 0),
+            Emoticon(1, R.drawable.ic_emoticon_02, "몽실이", 0),
+            Emoticon(2, R.drawable.ic_emoticon_03, "몽실이", 0),
+            Emoticon(3, R.drawable.ic_emoticon_04, "몽실이", 0),
+            Emoticon(4, R.drawable.ic_emoticon_05, "몽실이", 0),
+            Emoticon(5, R.drawable.ic_emoticon_06, "몽실이", 0),
+            Emoticon(6, R.drawable.ic_emoticon_07, "몽실이", 0),
+            Emoticon(7, R.drawable.ic_emoticon_08, "몽실이", 0),
+            Emoticon(8, R.drawable.ic_emoticon_09, "몽실이", 0),
+        )
+    }
+}
