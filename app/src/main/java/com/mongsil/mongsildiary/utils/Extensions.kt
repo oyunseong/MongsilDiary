5d8a402cf3292d1ef85f867a6af85ae52a97debf
oyunseong <gkrlsanjf@naver.com>
2022-07-14 18:55:25 +0900

    TimeSlot converter 추가

--- app/src/main/java/com/mongsil/mongsildiary/utils/Extensions.kt
+++ app/src/main/java/com/mongsil/mongsildiary/utils/Extensions.kt
@@ -3,6 +3,7 @@ package com.mongsil.mongsildiary.utils
 import android.content.Context
 import android.util.Log
 import android.widget.Toast
+import com.mongsil.mongsildiary.data.database.entity.TimeSlot
 
 fun String.printLog(tag: String? = null) {
     Log.d("+++ $tag", this)
@@ -14,4 +15,18 @@ fun Throwable.printError(tag: String? = null, message: String? = null) {
 
 fun Context.showToast(message: String) {
     Toast.makeText(this, message, Toast.LENGTH_SHORT).show()
+}
+
+fun TimeSlot.converterTimeSlot(): String {
+    return if (this == TimeSlot.Morning) {
+        "아침"
+    } else if (this == TimeSlot.Launch) {
+        "점심"
+    } else if(this == TimeSlot.Dinner){
+        "저녁"
+    }else if (this == TimeSlot.EndOfTheDay){
+        "하루끝"
+    }else{
+        ""
+    }
 }
